a1a436492f7fe6ac45ee589c158ddebb9b40e6fe
tuge <tuge@wedochina.cn>
2020-08-12 17:33:34 +0800

    test view single question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
     function index () {
 
+    }
+
+    function show (Question $question) {
+        return view('questions.show', compact('question'));
     }
 
 }
